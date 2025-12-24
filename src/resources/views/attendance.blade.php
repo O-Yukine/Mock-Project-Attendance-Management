0ee96ff057a7e9b5ffb87f3ead7cb880a9cab627
Yukine <yukineriko@gmail.com>
2025-12-24 09:37:57 +0900

    スタッフ用勤怠登録画面のスタイル

--- src/resources/views/attendance.blade.php
+++ src/resources/views/attendance.blade.php
@@ -20,14 +20,17 @@
                 <input type="hidden" name="time" value="{{ now()->format('H:i') }}">
                 {{ $time }}
             </div>
-            <div class="attendance_submit">
+            <div class="attendance__submit">
                 @if ($status == '勤務外')
-                    <button class="attendance__submit" name="action" value="start_working" type="submit">出勤</button>
+                    <button class="attendance__submit--button" name="action" value="start_working"
+                        type="submit">出勤</button>
                 @elseif($status == '出勤中')
-                    <button class="attendance__submit" name="action" value="finish_working" type="submit">退勤</button>
-                    <button class="attendance__submit" name="action" value="break_start" type="submit">休憩入</button>
+                    <button class="attendance__submit--button" name="action" value="finish_working"
+                        type="submit">退勤</button>
+                    <button class="attendance__submit--button" name="action" value="break_start"
+                        type="submit">休憩入</button>
                 @elseif($status == '休憩中')
-                    <button class="attendance__submit" name="action" value="break_end" type="submit">休憩戻</button>
+                    <button class="attendance__submit--button" name="action" value="break_end" type="submit">休憩戻</button>
                 @elseif($status == '退勤済')
                     <p>お疲れ様でした。</p>
                 @endif
