d3dcb259b970e0ab5f43df6b9c4c9ce7dcaf80a9
Yukine <yukineriko@gmail.com>
2025-12-26 18:53:20 +0900

    管理者用勤怠詳細ページのスタイル修正

--- src/resources/views/admin/attendance_detail.blade.php
+++ src/resources/views/admin/attendance_detail.blade.php
@@ -20,35 +20,51 @@
                     </tr>
                     <tr class="detail__table--row">
                         <th class="detail__table--title">日付</th>
-                        <td class="detail__table--date"><input type="hidden" name="work_date"
-                                value="{{ $attendance->work_date->format('Y-m-d') }}">
-                            <span class="date__year">
-                                {{ $attendance->work_date->format('Y年') }}
-                            </span>
-                            <span class="date__md">
-                                {{ $attendance->work_date->format('m月d日') }}
-                            </span>
+                        <td class="detail__table--date">
+                            <div class="input-day"><input type="hidden" name="work_date"
+                                    value="{{ $attendance->work_date->format('Y-m-d') }}">
+                                <span class="date__year">
+                                    {{ $attendance->work_date->format('Y年') }}
+                                </span>
+                                <span class="date__md">
+                                    {{ $attendance->work_date->format('m月d日') }}
+                                </span>
+                            </div>
                         </td>
                     </tr>
                     <tr class="detail__table--row">
                         <th class="detail__table--title">出勤・退勤</th>
-                        <td class="detail__table--date"><input type="text" name="clock_in"
-                                value="{{ optional($attendance->clock_in)->format('H:i') }}">
-                            〜
-                            <input type="text" name="clock_out"
-                                value="{{ optional($attendance->clock_out)->format('H:i') }}">
+                        <td class="detail__table--date">
+                            <div class="input-row"><input type="text" name="clock_in"
+                                    value="{{ optional($attendance->clock_in)->format('H:i') }}">
+                                〜
+                                <input type="text" name="clock_out"
+                                    value="{{ optional($attendance->clock_out)->format('H:i') }}">
+                            </div>
+                            @error('clock_in')
+                                <div class="form__error">{{ $message }}</div>
+                            @enderror
                         </td>
                     </tr>
                     @foreach ($attendance->breaks as $index => $break)
                         <tr class="detail__table--row">
                             <th class="detail__table--title">休憩{{ $index + 1 }}</th>
-                            <td class="detail__table--date"><input type="hidden" name="breaks[{{ $index }}][id]"
-                                    value="{{ $break->id }}">
-                                <input type="text" name="breaks[{{ $index }}][break_start]"
-                                    value="{{ optional($break->break_start)->format('H:i') }}">
-                                〜
-                                <input type="text" name="breaks[{{ $index }}][break_end]"
-                                    value="{{ optional($break->break_end)->format('H:i') }}">
+                            <td class="detail__table--date">
+                                <div class="input-row">
+                                    <input type="hidden" name="breaks[{{ $index }}][id]"
+                                        value="{{ $break->id }}">
+                                    <input type="text" name="breaks[{{ $index }}][break_start]"
+                                        value="{{ optional($break->break_start)->format('H:i') }}">
+                                    〜
+                                    <input type="text" name="breaks[{{ $index }}][break_end]"
+                                        value="{{ optional($break->break_end)->format('H:i') }}">
+                                </div>
+                                @error("breaks.$index.break_start")
+                                    <div class="form__error">{{ $message }}</div>
+                                @enderror
+                                @error("breaks.$index.break_end")
+                                    <div class="form__error">{{ $message }}</div>
+                                @enderror
                             </td>
                         </tr>
                     @endforeach
@@ -57,16 +73,21 @@
                     @endphp
                     <tr class="detail__table--row">
                         <th class="detail__table--title">休憩{{ $newInput + 1 }}</th>
-                        <td class="detail__table--date"> <input type="text"
-                                name="breaks[{{ $newInput }}][break_start]">
-                            〜
-                            <input type="text" name="breaks[{{ $newInput }}][break_end]">
+                        <td class="detail__table--date">
+                            <div class="input-row">
+                                <input type="text" name="breaks[{{ $newInput }}][break_start]">
+                                〜
+                                <input type="text" name="breaks[{{ $newInput }}][break_end]">
+                            </div>
                         </td>
                     </tr>
                     <tr class="detail__table--row">
                         <th class="detail__table--title">備考</th>
                         <td class="detail__table--date">
                             <textarea name="reason"></textarea>
+                            @error('reason')
+                                <div class="form__error">{{ $message }}</div>
+                            @enderror
                         </td>
                     </tr>
                 </table>
