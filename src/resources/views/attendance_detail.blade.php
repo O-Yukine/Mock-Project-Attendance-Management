bd5082738d9d01f91dcaa2774e1ec8817753923d
Yukine <yukineriko@gmail.com>
2025-12-26 18:05:18 +0900

    スタッフ用勤怠詳細ページのエラーメッセージ表示時のスタイル修正

--- src/resources/views/attendance_detail.blade.php
+++ src/resources/views/attendance_detail.blade.php
@@ -19,23 +19,28 @@
                     </tr>
                     <tr class="detail__table--row">
                         <th class="detail__table--title">日付</th>
-                        <td class="detail__table--day"><input type="hidden" name="work_date"
-                                value="{{ $attendance->work_date->format('Y-m-d') }}">
-                            <span class="date__year">
-                                {{ $attendance->work_date->format('Y年') }}
-                            </span>
-                            <span class="date__md">
-                                {{ $attendance->work_date->format('m月d日') }}
-                            </span>
+                        <td class="detail__table--date">
+                            <div class="input-day"><input type="hidden" name="work_date"
+                                    value="{{ $attendance->work_date->format('Y-m-d') }}">
+                                <span class="date__year">
+                                    {{ $attendance->work_date->format('Y年') }}
+                                </span>
+                                <span class="date__md">
+                                    {{ $attendance->work_date->format('m月d日') }}
+                                </span>
+                            </div>
                         </td>
                     </tr>
                     <tr class="detail__table--row">
                         <th class="detail__table--title">出勤・退勤</th>
-                        <td class="detail__table--date"><input type="text" name="clock_in"
-                                value="{{ old('clock_in', optional($attendance->clock_in)->format('H:i')) }}">
-                            〜
-                            <input type="text" name="clock_out"
-                                value="{{ old('clock_out', optional($attendance->clock_out)->format('H:i')) }}">
+                        <td class="detail__table--date">
+                            <div class="input-row">
+                                <input type="text" name="clock_in"
+                                    value="{{ old('clock_in', optional($attendance->clock_in)->format('H:i')) }}">
+                                〜
+                                <input type="text" name="clock_out"
+                                    value="{{ old('clock_out', optional($attendance->clock_out)->format('H:i')) }}">
+                            </div>
                             @error('clock_in')
                                 <div class="form__error">{{ $message }}</div>
                             @enderror
@@ -44,20 +49,22 @@
                     @foreach ($attendance->breaks as $index => $break)
                         <tr class="detail__table--row">
                             <th class="detail__table--title">休憩{{ $index + 1 }}</th>
-                            <td class="detail__table--date"><input type="hidden" name="breaks[{{ $index }}][id]"
-                                    value="{{ $break->id ?? '' }}">
-                                <input type="text" name="breaks[{{ $index }}][break_start]"
-                                    value="{{ old("breaks.$index.break_start", optional($break->break_start)->format('H:i')) }}">
-                                〜
-                                <input type="text" name="breaks[{{ $index }}][break_end]"
-                                    value="{{ old("breaks.$index.break_end", optional($break->break_end)->format('H:i')) }}">
+                            <td class="detail__table--date">
+                                <div class="input-row">
+                                    <input type="hidden" name="breaks[{{ $index }}][id]"
+                                        value="{{ $break->id ?? '' }}">
+                                    <input type="text" name="breaks[{{ $index }}][break_start]"
+                                        value="{{ old("breaks.$index.break_start", optional($break->break_start)->format('H:i')) }}">
+                                    〜
+                                    <input type="text" name="breaks[{{ $index }}][break_end]"
+                                        value="{{ old("breaks.$index.break_end", optional($break->break_end)->format('H:i')) }}">
+                                </div>
                                 @error("breaks.$index.break_start")
                                     <div class="form__error">{{ $message }}</div>
                                 @enderror
                                 @error("breaks.$index.break_end")
                                     <div class="form__error">{{ $message }}</div>
                                 @enderror
-
                             </td>
                         </tr>
                     @endforeach
