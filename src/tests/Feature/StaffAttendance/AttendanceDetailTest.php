3850fea3c70d06e87544aac2160c1a193699121b
Yukine <yukineriko@gmail.com>
2026-01-05 16:31:56 +0900

    一般ユーザー勤怠詳細情報取得機能のテストコード

--- src/tests/Feature/StaffAttendance/AttendanceDetailTest.php
+++ src/tests/Feature/StaffAttendance/AttendanceDetailTest.php
@@ -3,20 +3,109 @@
 namespace Tests\Feature\StaffAttendance;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Attendance;
+use App\Models\BreakTime;
+use Carbon\Carbon;
 
 class AttendanceDetailTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_user_name_is_in_detail_page()
+    {
+        Carbon::setTestNow(Carbon::parse('2025-12-03'));
+
+        $user = User::factory()->create();
+
+        $attendance = Attendance::create([
+            'user_id' => $user->id,
+            'work_date' => '2025-12-01',
+            'clock_in' => '09:00',
+            'clock_out' => '18:00',
+            'status' => 'clock_out',
+        ]);
+        BreakTime::create([
+            'attendance_id' => $attendance->id,
+            'break_start' => '12:00',
+            'break_end' => '13:00',
+        ]);
+
+        $this->actingAs($user)
+            ->get('/attendance/detail/' . $attendance->id)
+            ->assertSee($user->name);
+    }
+    public function test_detail_page_is_showing_choosen_day()
+    {
+        Carbon::setTestNow(Carbon::parse('2025-12-03'));
+
+        $user = User::factory()->create();
+
+        $attendance = Attendance::create([
+            'user_id' => $user->id,
+            'work_date' => '2025-12-01',
+            'clock_in' => '09:00',
+            'clock_out' => '18:00',
+            'status' => 'clock_out',
+        ]);
+        BreakTime::create([
+            'attendance_id' => $attendance->id,
+            'break_start' => '12:00',
+            'break_end' => '13:00',
+        ]);
+
+        $this->actingAs($user)
+            ->get('/attendance/detail/' . $attendance->id)
+            ->assertSee('2025年')
+            ->assertSee('12月01日');
+    }
+    public function test_user_attendance_time_is_in_detail_page()
+    {
+        Carbon::setTestNow(Carbon::parse('2025-12-03'));
+
+        $user = User::factory()->create();
+
+        $attendance = Attendance::create([
+            'user_id' => $user->id,
+            'work_date' => '2025-12-01',
+            'clock_in' => '09:00',
+            'clock_out' => '18:00',
+            'status' => 'clock_out',
+        ]);
+        BreakTime::create([
+            'attendance_id' => $attendance->id,
+            'break_start' => '12:00',
+            'break_end' => '13:00',
+        ]);
+
+        $this->actingAs($user)
+            ->get('/attendance/detail/' . $attendance->id)
+            ->assertSee('09:00')
+            ->assertSee('18:00');
+    }
+    public function test_user_break_time_is_in_detail_page()
+    {
+        Carbon::setTestNow(Carbon::parse('2025-12-03'));
+
+        $user = User::factory()->create();
+
+        $attendance = Attendance::create([
+            'user_id' => $user->id,
+            'work_date' => '2025-12-01',
+            'clock_in' => '09:00',
+            'clock_out' => '18:00',
+            'status' => 'clock_out',
+        ]);
+        BreakTime::create([
+            'attendance_id' => $attendance->id,
+            'break_start' => '12:00',
+            'break_end' => '13:00',
+        ]);
+
+        $this->actingAs($user)
+            ->get('/attendance/detail/' . $attendance->id)
+            ->assertSee('12:00')
+            ->assertSee('13:00');
     }
 }
