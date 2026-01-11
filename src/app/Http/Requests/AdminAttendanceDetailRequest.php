e653f8a093ef7927a0e94cf458e401c7ebc2fb52
Yukine <yukineriko@gmail.com>
2026-01-12 07:57:19 +0900

    adminattendancedetailrequestの修正

--- src/app/Http/Requests/AdminAttendanceDetailRequest.php
+++ src/app/Http/Requests/AdminAttendanceDetailRequest.php
@@ -27,9 +27,9 @@ class AdminAttendanceDetailRequest extends FormRequest
             'clock_in' => ['required', 'date_format:H:i', 'before:clock_out'],
             'clock_out' => ['required', 'date_format:H:i'],
             'reason' => ['required', 'string'],
-            'breaks.*.break_start' => ['nullable', 'after:clock_in', 'before:clock_out'],
-            'breaks.*.break_end' => ['nullable', 'before:clock_out'],
-
+            'breaks.*.id'          => ['nullable', 'integer', 'exists:break_times,id'],
+            'breaks.*.break_start' => ['nullable', 'date_format:H:i', 'after:clock_in', 'before:clock_out'],
+            'breaks.*.break_end' => ['nullable', 'date_format:H:i', 'before:clock_out', 'after_or_equal:breaks.*.break_start',],
         ];
     }
 
@@ -37,11 +37,18 @@ class AdminAttendanceDetailRequest extends FormRequest
     {
         return [
             'clock_in.before' => '出勤時間もしくは退勤時間が不適切な値です',
+            'clock_in.date_format' => '出勤時間もしくは退勤時間の形式が不正です',
+            'clock_out.date_format' => '出勤時間もしくは退勤時間の形式が不正です',
+
             'reason.required' => '備考を記入してください',
 
             'breaks.*.break_start.after' => '休憩時間が不適切な値です',
             'breaks.*.break_start.before' => '休憩時間が不適切な値です',
+            'breaks.*.break_start.date_format' => '休憩開始時間の形式が不正です',
+
             'breaks.*.break_end.before' => '休憩時間もしくは退勤時間が不適切な値です',
+            'breaks.*.break_end.date_format' => '休憩終了時間の形式が不正です',
+            'breaks.*.break_end.after_or_equal' => '休憩終了時間は休憩開始時間以降である必要があります',
 
         ];
     }
